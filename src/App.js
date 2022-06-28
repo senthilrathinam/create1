19542a1c7145ef101a9a90c89bd86c2babb7df0f
skamalar <kr_senthil_kumar@yahoo.co.in>
2022-06-28 15:14:24 -0400

    commit3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-    <h1>This is my deployed appss</h1>
+    <>
+      <h1>This is my deployed app abc</h1>
+      <h1>Hello {user.username}</h1>
+
        <h2>Example.com - Employee Portal</h2>
         <h3>Current shipment stats</h3>
             <iframe
@@ -12,23 +19,10 @@ function App() {
             height="720"
             src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/306802105982/dashboards/f4d6ae71-0e57-471b-877a-8f598785a670?directory_alias=senthil-quicksight-account1">
         </iframe>
-      {/*
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-    </div>
+
+      <h2><button onClick={signOut}>Sign out</button></h2>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
