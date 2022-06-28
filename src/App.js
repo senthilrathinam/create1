228d466fe84ebe1eebdead3bbe9eae41e4496b77
skamalar <kr_senthil_kumar@yahoo.co.in>
2022-06-28 12:41:56 -0400

    commit1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>This is my deployed app</h1>
+    <h1>This is my deployed apps</h1>
       {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
