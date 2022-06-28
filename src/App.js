5978b963a18541721ecc4aa7b65cbf92752cad48
skamalar <kr_senthil_kumar@yahoo.co.in>
2022-06-28 12:47:16 -0400

    commit2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>This is my deployed apps</h1>
+    <h1>This is my deployed appss</h1>
+       <h2>Example.com - Employee Portal</h2>
+        <h3>Current shipment stats</h3>
+            <iframe
+            width="960"
+            height="720"
+            src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/306802105982/dashboards/f4d6ae71-0e57-471b-877a-8f598785a670?directory_alias=senthil-quicksight-account1">
+        </iframe>
       {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
